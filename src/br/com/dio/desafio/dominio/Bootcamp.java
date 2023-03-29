2387893e67b9038757f2796388ffc82d8f7a6d06
mancinilucas <mancini.lucasg@gmail.com>
2023-03-29 13:17:07 -0300

    Bootcamp e Dev classes criadas

---
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -0,0 +1,54 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Bootcamp {
+    private String nome;
+    private String descricao;
+    private final LocalDate dataInicial = LocalDate.now();
+    private final LocalDate dataFinal = dataInicial.plusDays(45);
+    private Set<Dev> devsInscritos = new HashSet<>();
+    private Set<Conteudo> conteudos = new LinkedHashSet<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataInicial() {
+        return dataInicial;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Bootcamp bootcamp = (Bootcamp) o;
+
+        return getNome() != null ? getNome().equals(bootcamp.getNome()) : bootcamp.getNome() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getNome() != null ? getNome().hashCode() : 0;
+    }
+}
---
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,63 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Dev dev = (Dev) o;
+
+        if (!Objects.equals(nome, dev.nome)) return false;
+        if (!Objects.equals(conteudosInscritos, dev.conteudosInscritos))
+            return false;
+        return Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = nome != null ? nome.hashCode() : 0;
+        result = 31 * result + (conteudosInscritos != null ? conteudosInscritos.hashCode() : 0);
+        result = 31 * result + (conteudosConcluidos != null ? conteudosConcluidos.hashCode() : 0);
+        return result;
+    }
+
+    public void inscreverBootcamp(Bootcamp bootcamp){}
+
+    public void progredir(){}
+
+    public void calcularXp(){}
+}
