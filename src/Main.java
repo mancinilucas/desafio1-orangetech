eb4152441f7264a7fc9d753dd3609a29d34c9dbf
mancinilucas <mancini.lucasg@gmail.com>
2023-03-29 11:22:10 -0300

    Implementação de classes Curso e Mentoria Criação de Readme

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso("curso java", "descricao curso java", 8);
+        Curso curso2 = new Curso("curso javascript", "descricao curso javascript", 4);
+
+        Mentoria mentoria1 = new Mentoria("mentoria de java", "descricao mentoria java", LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
     }
 }
---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,63 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    private String titulo;
+    private String descricao;
+    private LocalDate data;
+
+    public Mentoria(){}
+
+    public Mentoria(String titulo, String descricao, LocalDate data) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.data = data;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", data=" + data +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Mentoria mentoria)) return false;
+
+        return getTitulo().equals(mentoria.getTitulo());
+    }
+
+    @Override
+    public int hashCode() {
+        return getTitulo().hashCode();
+    }
+}
